df90733d2ba7058415ccf65bfe988539ca075b67
robert <robertmoga16@gmail.com>
2021-10-25 22:08:58 +0300

    Created blog app

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'title': 'Post1',
+        'author': 'Gustav',
+        'date': '23 Aug 1944',
+        'content': 'super content'
+    },
+    {
+        'title': 'Post2',
+        'author': 'Janeta',
+        'date': '14 Feb 1998',
+        'content': 'Rav4'
+    }
+
+]
 
 def home(request):
-    return HttpResponse('<h1> Blog Home </h1>')
+    context = {'posts': posts}
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1> About blog </h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
