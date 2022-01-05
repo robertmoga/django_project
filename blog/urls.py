e17ef500587993df49d37db8b6efd5dd831c9563
robert <robertmoga16@gmail.com>
2022-01-05 14:33:56 +0200

    Added ClassBasedViews and Paginator

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
-from blog.views import home, about
+from blog.views import about
+from blog.views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, UserPostListView
 
 urlpatterns = [
-    path('', home, name='blog-home'),
+    path('', PostListView.as_view(), name='blog-home'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/<str:username>/', UserPostListView.as_view(), name='user-posts'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('about/', about, name='blog-about')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,65 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
+from django.contrib.auth.models import User
 from blog.models import Post
 
 def home(request):
     context = {'posts': Post.objects.all()}
     return render(request, 'blog/home.html', context)
+
+
+class PostListView(ListView):
+    model = Post
+    context_object_name = 'posts'
+    template_name = 'blog/home.html'
+    ordering = ['-date_posted']
+    paginate_by = 2
+
+
+class UserPostListView(ListView):
+    model = Post
+    context_object_name = 'posts'
+    template_name = 'blog/user_posts.html'
+    paginate_by = 2
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content'] # fields that we want in the form
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        return self.request.user == self.get_object().author
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
+
+    def test_func(self):
+        return self.request.user == self.get_object().author
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
