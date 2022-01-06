c80846a3407d3d439672e040f4d8b42cb2a1e21f
robert <robertmoga16@gmail.com>
2022-01-06 13:28:37 +0200

    Implemented the password reset functionality
    ENV vars must be set for credentials, check settings.py

--- django_project/urls.py
+++ django_project/urls.py
@@ -18,7 +18,9 @@ from django.urls import path, include
 from users.views import user_registration, profile
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import (LoginView, LogoutView, PasswordResetView,
+                                       PasswordResetDoneView, PasswordResetConfirmView,
+                                       PasswordResetCompleteView)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,7 +28,11 @@ urlpatterns = [
     path('register/', user_registration, name='register'),
     path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-    path('profile/', profile, name='profile')
+    path('profile/', profile, name='profile'),
+    path('password-reset', PasswordResetView.as_view(template_name='users/password_reset.html'), name='password_reset'),
+    path('password-reset/done/', PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>', PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('password-reset/complete/', PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'), name='password_reset_complete')
 ]
 
 if settings.DEBUG:
